d89724d1440f380cf09b2c8fe6de8728c8dccbb0
Fatih Erikli <fatiherikli@gmail.com>
2017-02-07 02:41:07 +0100

    Visual tweaks

--- src/Container.js
+++ src/Container.js
@@ -38,7 +38,7 @@ export default class App extends Component {
     this.run = this.run.bind(this);
     this.handleReset = this.handleReset.bind(this);
     
-    const graph = randomgraph.ErdosRenyi.np(200, 0.008);
+    const graph = randomgraph.ErdosRenyi.np(270, 0.008);
     const avarageConsciousness = 0.2;
 
     this.state = {
